6ff2a6354656744276a361b146d43ec16a8a5c04
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-08-24 17:33:05 -0300

    updating folders

--- afro-academy/2_portfolio/index.js
+++
@@ -1,8 +0,0 @@
-const showMenu = document.querySelector("#burguer");
-const menu = document.querySelector("#navigation .menu");
-const burg = document.querySelector("#navigation .burguer");
-
-showMenu.addEventListener("click", () => {
-  menu.classList.toggle("show");
-  burg.classList.toggle("show");
-});
