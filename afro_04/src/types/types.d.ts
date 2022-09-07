a1fb053a27e9e6ba891e467098e4ff5d2b2a0d14
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2022-09-07 16:36:23 -0300

    refactor: add type

--- afro_04/src/types/types.d.ts
+++ afro_04/src/types/types.d.ts
@@ -1,9 +1,13 @@
 interface Item {
   text: string;
+  date: string;
 }
 
 interface SingleElementProps {
   item: Item;
   position: number;
   removeItem: (n: number) => void;
+  handleTask: (isChecked: boolean) => void;
 }
+
+type TextStyle = "text-normal" | "text-line";
