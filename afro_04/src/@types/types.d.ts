4eff82545cbae418ed79c79468ca94be1a6b9c0b
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-09-12 12:26:03 -0300

    fix: update types

--- afro_04/src/@types/types.d.ts
+++ afro_04/src/@types/types.d.ts
@@ -2,14 +2,36 @@ interface Item {
   text: string;
   date: string;
   isChecked: boolean;
+  isOnEdit: boolean;
+  endDate?: string;
+  index?: number;
 }
 
 interface SingleElementProps {
   item: Item;
-  position: number;
-  updateItemText: (n: number, newValue: string) => void;
+  index: number;
+  loadSingleItem: (n: number) => void;
   removeItem: (n: number) => void;
   handleTask: (n: number, isChecked: boolean) => void;
 }
 
 type TextStyle = "text-normal" | "text-line";
+type ElementEditStyle = {
+  box: "border-normal" | "border-edit";
+  icon: "icon-normal" | "icon-edit";
+};
+
+interface FormProps {
+  handleItem: (item: Item) => void;
+}
+
+interface EditItemProps {
+  editItem: Item | null;
+}
+
+interface CounterProps {
+  checkedItems: number;
+  arraySize: number;
+}
+
+type DateFormat = "dd/MM/yyyy" | "dd/MM/yy";
